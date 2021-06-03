6bab60e561f62e3a2d6a1605faa8751243046ca7
Joep Geuskens <joep.geuskens@gmail.com>
2021-06-03 22:44:32 +0200

    Added support for numpy arrays and DataFrames

--- example.py
+++ example.py
@@ -2,22 +2,32 @@
 """
 @author: Joep Geuskens
 """
-from CSVToLatex import CSVToLaTeX
+from ToLaTeXTable import LaTeXTable
 import numpy as np
-
+from pandas import DataFrame
 
 file_name = "test.csv"
-#create a test file
-a = np.random.random((30,10))*10
+#create a test dataset
+a = np.random.random((26,10))*10
+#safe it to a file
 np.savetxt(file_name, a, delimiter=",", header=",".join(["$x_{:d}$".format(i) for i in range(10)]))
+#create a dataframe out of it
+df = DataFrame(data=a, index=[c for c in "abcdefghijklmnopqrstuvwxyz"])
 
-ctl = CSVToLaTeX(file_name, ncols=10, header=True) #create the converter
+#create a latex table from the dataframe
+ctl = LaTeXTable(df, headers=["$x_{:d}$".format(i) for i in range(10)]) #create the converter
+#only needed for the dataframe (if an index is provided)
+ctl.set_index_formatter(lambda s: r"\textbf{"+s+"}") #make the first column bold
+
+#create a latex table from the dataset
+ctl = LaTeXTable(a, headers=["$x_{:d}$".format(i) for i in range(10)])
+#create a latex table from the csv file (if there is header=False, ncols MUST be given)
+ctl = LaTeXTable(file_name, ncols=10, header=True)
 
 ctl.set_formatters(lambda s: "{:.2f}".format(float(s))) #default formatter to convert strings to floats
 
-ctl.set_formatter(0, lambda s: r"\textbf{{ {:.2f} }}".format(float(s))) #make the first column bold
 
-#Optionally, set `header=False` on line 10 and instead use the line below to add custom headers!
+#Optionally, set custom headers!
 #ctl.set_headers(["$x$", "$y_1$", "$y_2$","$y_3$","$y_4$","$y_5$","$y_6$","$y_7$","$y_8$","$y_9$"]) #optionally add 'bold=True' for a bold header
 
 #more custom stuff
